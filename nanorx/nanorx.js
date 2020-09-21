6f3767ae1a4b123fdd5d403132d84aa5112d55b4
Igor Malovitsa <igor.malovitsa@gmail.com>
2020-09-21 17:45:58 +0300

    documented nanorx

--- nanorx/nanorx.js
+++ nanorx/nanorx.js
@@ -1,6 +1,10 @@
-// LICENSE: UNLICENSED
-// FOR DEMONSTRATIONAL PURPOSES ONLY
-export const debounce = (threshold=200, fireFirst=false) => (fn) => {
+/** @license: UNLICENSED
+FOR DEMONSTRATIONAL PURPOSES ONLY*/
+
+// debounce: hold off consecutive events if they happened
+// with a time interval smaller than threshold milliseconds.
+// allowLeading allows the leading event to pass through.
+export const debounce = (threshold=200, allowLeading=false) => (fn) => {
     const Idle = 0;
     const Cooldown = 1;
     const Pending = 2;
@@ -23,7 +27,7 @@ export const debounce = (threshold=200, fireFirst=false) => (fn) => {
     };
 
     return (value) => {
-        if (state === Idle && fireFirst) {
+        if (state === Idle && allowLeading) {
             state = Cooldown;
             toHandle = setTimeout(relax, threshold);
             fn(value);
@@ -36,6 +40,9 @@ export const debounce = (threshold=200, fireFirst=false) => (fn) => {
     };
 };
 
+// switchPromise is similar to Rx.js switchMap.
+// Transforms a stream of promises into a stream
+// of most recent values (or errors).
 export const switchPromise = (resolver) => (fn) => {
     let index = 0;
     let receivedIndex = -1;
@@ -49,14 +56,16 @@ export const switchPromise = (resolver) => (fn) => {
             fn(value);
         };
         index += 1;
-        resolver(value)
-            .then((result) => handleResult({result}))
-            .catch((error) => handleResult({error}));
+        resolver(value).then((result) => handleResult({result}))
+             .catch((error) => handleResult({error}));
     };
 };
 
 const shallowEq = (a, b) => a === b;
 const identity = x => x;
+// Removes repeating values from the stream.
+// By default, uses strict shallow equality.
+// key allows specifying a mapper for uniqueness criteria.
 export const distinctUntilChanged = (eq=shallowEq, key=identity) => (fn) => {
     let hasPrevious = false;
     let previous;
@@ -71,8 +80,11 @@ export const distinctUntilChanged = (eq=shallowEq, key=identity) => (fn) => {
     };
 };
 
+// Transform a stream of values using a mapper function
 export const map = (mapper) => (fn) => (value) => fn(mapper(value));
 
+// Apply multiple stream transformations and attach it to a stream listener.
+// stream listener is the last argument.
 export const pipe = (...args) => {
     let index = args.length - 1;
     let fn = args[index];
